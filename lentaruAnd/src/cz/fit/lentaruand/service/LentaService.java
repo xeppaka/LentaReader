4004ca09a62ad62b32ff98992e18e8658c4b6cbc
TheWalkingDelirium <george.mukhammed@gmail.com>
2013-08-06 17:23:28 +0200

    LentaService now extends Service (extended IntentService before)

--- lentaruAnd/src/cz/fit/lentaruand/service/LentaService.java
+++ lentaruAnd/src/cz/fit/lentaruand/service/LentaService.java
@@ -1,10 +1,15 @@
 package cz.fit.lentaruand.service;
 
-import android.app.IntentService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
+import android.os.Process;
 import android.os.ResultReceiver;
-import android.text.TextUtils;
 import android.util.Log;
+import android.util.SparseArray;
 import android.widget.Toast;
 import cz.fit.lentaruand.data.Rubrics;
 import cz.fit.lentaruand.utils.LentaConstants;
@@ -26,7 +31,10 @@ import cz.fit.lentaruand.utils.LentaConstants;
  * @see Processor
  */
 
-public class LentaService extends IntentService {
+public class LentaService extends Service {
+	
+	private static final int NUM_THREADS = 4;
+	 
 	public static final String PACKAGE = "cz.fit.lentaruand.service";
 	public static final String ACTION_EXECUTE_COMMAND = PACKAGE.concat(".ACTION_EXECUTE_COMMAND");
 	public static final String ACTION_EXECUTE_DOWNLOAD_BRIEF = PACKAGE.concat(".ACTION_EXECUTE_DOWNLOAD_BRIEF");
@@ -38,45 +46,101 @@ public class LentaService extends IntentService {
 	public static final String EXTRA_RUBRIC = PACKAGE.concat(".EXTRA_RUBRIC");
 	public static final String EXTRA_LIST = PACKAGE.concat(".EXTRA_LIST");
 	
+	private ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS); // поменять потом очередь приоритетов
+	private SparseArray<RunningCommand> runningCommands = new SparseArray<RunningCommand>();
 	Processor processor = new Processor(this);
-	
-	public LentaService(){
-		super("Lenta Download Service");
+		
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		
+		if (ACTION_EXECUTE_DOWNLOAD_BRIEF.equals(intent.getAction())) {
+			RunningCommand runningCommand = new RunningCommand(intent);
+			synchronized (runningCommands) {
+				runningCommands.append(getCommandId(intent), runningCommand);
+			}
+			executor.submit(runningCommand);
+		}
+//		if (ACTION_CANCEL_COMMAND.equals(intent.getAction())) {
+//			RunningCommand runningCommand = runningCommands
+//					.get(getCommandId(intent));
+//			if (runningCommand != null) {
+//				runningCommand.cancel();
+//			}
+//		}
+		return START_NOT_STICKY;
 	}
 	
 	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-	    Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-	    Log.d(LentaConstants.LoggerServiceTag, "Service started!");
-	    return super.onStartCommand(intent,flags,startId);
+	public IBinder onBind(Intent arg0) {
+		return null;
 	}
+	
+	@Override
+	public void onCreate() {
+		Toast.makeText(this, "service created", Toast.LENGTH_SHORT).show();
+		Log.d(LentaConstants.LoggerServiceTag, "Service created!");
+
+		super.onCreate();
+	}
+	
+	@Override
+    public void onDestroy() {
+		super.onDestroy();
+	}
+	
+	private class RunningCommand implements Runnable {
+
+		private Intent intent;
+
+		public RunningCommand(Intent intent) {
+			this.intent = intent;
+		}
+
+		public void cancel() {
+		}
+
+		@Override
+		public void run() {
+			Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+			Log.d(LentaConstants.LoggerServiceTag, "starting download");
+//			processor.downloadRubricBrief(getRubric(intent), getReceiver(intent)); 
+			processor.execute(intent);
+			Log.d(LentaConstants.LoggerServiceTag, "Done download.");
+			shutdown();
+		}
+
+		private void shutdown() {
+			synchronized (runningCommands) {
+				runningCommands.remove(getCommandId(intent));
+				if (runningCommands.size() == 0) {
+					stopSelf();
+				}
+			}
+		}
+
+	}
+	
+	private int getCommandId(Intent intent) {
+		return intent.getIntExtra(EXTRA_REQUEST_ID, -1);
+	}
+	
 	/**
 	 * The IntentService calls this method from the default worker thread with
 	 * the intent that started the service. When this method returns,
 	 * IntentService stops the service, as appropriate.
 	 */
-	@Override
-	protected void onHandleIntent(Intent intent) {
-
-		Log.d(LentaConstants.LoggerServiceTag, "Handled intent!");
-		String action = intent.getAction();
-		if (!TextUtils.isEmpty(action)) {
-			if(action == ACTION_EXECUTE_DOWNLOAD_BRIEF){
-				Log.d(LentaConstants.LoggerServiceTag, "starting download");
-				processor.downloadRubricBrief(getRubric(intent), getReceiver(intent)); // 
-				Log.d(LentaConstants.LoggerServiceTag, "Done. Stopping myself");
-				stopSelf();				 
-			}
-		}
-	}
-	
-	private ResultReceiver getReceiver(Intent intent) {
-		return intent.getParcelableExtra(EXTRA_STATUS_RECEIVER);
-	}
-
-	private Rubrics getRubric(Intent intent) {
-		Rubrics rubric = (Rubrics) intent.getSerializableExtra(EXTRA_RUBRIC);
-		if(rubric == null) Log.d(LentaConstants.LoggerServiceTag, "rubric is null");
-		return rubric;
-	}
+//	@Override
+//	protected void onHandleIntent(Intent intent) {
+//
+//		Log.d(LentaConstants.LoggerServiceTag, "Handled intent!");
+//		String action = intent.getAction();
+//		if (!TextUtils.isEmpty(action)) {
+//			if(action == ACTION_EXECUTE_DOWNLOAD_BRIEF){
+//				Log.d(LentaConstants.LoggerServiceTag, "starting download");
+//				processor.downloadRubricBrief(getRubric(intent), getReceiver(intent)); // 
+//				Log.d(LentaConstants.LoggerServiceTag, "Done. Stopping myself");
+//				stopSelf();				 
+//			}
+//		}
+//	}
 }
--- lentaruAnd/src/cz/fit/lentaruand/service/Processor.java
+++ lentaruAnd/src/cz/fit/lentaruand/service/Processor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.util.Log;
@@ -84,6 +85,23 @@ public class Processor {
 		if (resultReceiver != null) {
 			resultReceiver.send(resultCode, data);
 		}
+	}
+
+	public void execute(Intent intent) {
+		if(intent.getAction().equals(LentaService.ACTION_EXECUTE_DOWNLOAD_BRIEF)){
+			downloadRubricBrief(getRubric(intent), getReceiver(intent));
+		}
+		
+	}
+	
+	private ResultReceiver getReceiver(Intent intent) {
+		return intent.getParcelableExtra(LentaService.EXTRA_STATUS_RECEIVER);
+	}
+
+	private Rubrics getRubric(Intent intent) {
+		Rubrics rubric = (Rubrics) intent.getSerializableExtra(LentaService.EXTRA_RUBRIC);
+		if(rubric == null) Log.d(LentaConstants.LoggerServiceTag, "rubric is null");
+		return rubric;
 	}
 
 }
