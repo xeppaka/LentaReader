ae244a4145bc33154ca669f4eb26157c6fb4f8e1
Kachalouski Pavel <xeppaka@gmail.com>
2013-08-26 17:56:53 +0200

    Started implementation for getting news objects for rubric

---
+++ lentaruAnd/src/cz/fit/lentaruand/data/dao/NewsObjectsDao.java
@@ -0,0 +1,18 @@
+package cz.fit.lentaruand.data.dao;
+
+import java.util.Collection;
+
+import cz.fit.lentaruand.data.NewsObject;
+import cz.fit.lentaruand.data.Rubrics;
+
+/**
+ * DAO interface that should implement every DAO implementation that represents
+ * News, Articles, etc.
+ * 
+ * @author nnm
+ * 
+ * @param <T>
+ */
+public interface NewsObjectsDao<T extends NewsObject> extends Dao<T> {
+	Collection<T> readForRubric(Rubrics rubric);
+}
