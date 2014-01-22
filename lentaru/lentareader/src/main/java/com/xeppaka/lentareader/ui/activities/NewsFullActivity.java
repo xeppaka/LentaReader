c95301a40dbbdc7cda2789d98ff5bbbdba6bbe94
Kachalouski Pavel <xeppaka@gmail.com>
2014-01-22 18:38:03 +0100

    Image placeholder changed. Messages changed.

--- lentaru/lentareader/src/main/java/com/xeppaka/lentareader/ui/activities/NewsFullActivity.java
+++ lentaru/lentareader/src/main/java/com/xeppaka/lentareader/ui/activities/NewsFullActivity.java
@@ -19,6 +19,7 @@ public class NewsFullActivity extends ActionBarActivity {
         setTitle(null);
         //getActionBar().setIcon(R.drawable.lenta_icon);
         getSupportActionBar().setLogo(R.drawable.ab_lenta_icon);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 		setContentView(R.layout.full_news_activity);
 		long newsId = getIntent().getLongExtra("newsId", -1);
