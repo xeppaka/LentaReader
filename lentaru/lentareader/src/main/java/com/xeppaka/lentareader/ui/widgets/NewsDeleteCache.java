5fbe653091da9d2e0911958b61f49571bce882a9
Pavel Kachalouski <xeppaka@gmail.com>
2014-01-25 15:26:57 +0100

    preferences work: doing about program

--- lentaru/lentareader/src/main/java/com/xeppaka/lentareader/ui/widgets/NewsDeleteCache.java
+++ lentaru/lentareader/src/main/java/com/xeppaka/lentareader/ui/widgets/NewsDeleteCache.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -29,9 +31,10 @@ public class NewsDeleteCache extends DialogPreference {
 
         final TextView textView = new TextView(getContext());
         textView.setText(R.string.news_clear_cache_confirmation);
+        textView.setGravity(Gravity.CENTER);
 
-        final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.gravity = Gravity.CENTER_VERTICAL;
+        final ViewGroup.MarginLayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        params.topMargin = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 18f, getContext().getResources().getDisplayMetrics());
         textView.setLayoutParams(params);
 
         layout.addView(textView);
