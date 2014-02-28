e3764aafc0b1a4eec361834db59ab1e54f6b57f1
Kachalouski Pavel <xeppaka@gmail.com>
2014-02-28 17:53:57 +0100

    working on comments.

---
+++ lentaru/lentareader/src/main/java/com/xeppaka/lentareader/ui/fragments/CommentsListFragment.java
@@ -0,0 +1,12 @@
+package com.xeppaka.lentareader.ui.fragments;
+
+import android.app.ListFragment;
+
+/**
+ * Created by kacpa01 on 2/28/14.
+ */
+public class CommentsListFragment extends ListFragment {
+    private String xid;
+
+
+}
